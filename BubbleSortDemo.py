7c94bf2da6f1d8fef4bf0bdc41c555279de3206b
Manish976 <shakyamanish976@gmail.com>
2020-03-07 13:28:23 +0545

    coded in bubble sort

--- BubbleSortDemo.py
+++ BubbleSortDemo.py
@@ -1,1 +1,11 @@
-print("this is bubble sort")
+list=[15,12,10,9,8,13]
+
+for i in range(len(list)):
+    print(i)
+
+    for j in range(0,len(list)-i-1):
+        if list[j]>list[j+1]:
+            list[j],list[j+1]=list[j+1],list[j]
+
+
+print(list)
